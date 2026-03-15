b7ed8ba639d4ba88f7cc16919caef3f408ecc1b1
xxf5vxx <akrammimouni2012@gmail.com>
2026-03-15 14:26:32 +0000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Khazouk Empire",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: 4, 
       },
     ],
   },
